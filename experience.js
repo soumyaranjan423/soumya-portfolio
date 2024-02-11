fc2309f0cb8eda7c216dc494d2c63500f04d8a1c
soumyaranjan423 <soumyaranjan.n@trikatechnologies.com>
2024-02-11 19:02:51 +0530

    first commit

---
+++ experience.js
@@ -0,0 +1,38 @@
+const experience = {
+    "company1": {
+      "name": "CHEMOL TECHNOLOGIES INDIA PRIVATE LIMITED",
+      "position": "Frontend Developer",
+      "startDate": "2020-01-15",
+      "endDate": "2022-02-20",
+      "responsibilities": [
+        "Developed responsive web applications using React.js and Next.js",
+        "Collaborated with design and backend teams to implement UI/UX designs",
+        "Optimized application performance through code refactoring and performance tuning"
+      ]
+    },
+    "company2": {
+      "name": " Bhavyansh InfoTech PVT.LTD",
+      "position": "Full Stack Developer",
+      "startDate": "2022-03-30",
+      "endDate": "2023-03-17",
+      "responsibilities": [
+        "Built RESTful APIs using Express.js and MongoDB for data storage",
+        "Implemented authentication and authorization mechanisms for secure user access",
+        "Worked on the migration of legacy systems to modern architectures"
+      ]
+    },
+    "company3": {
+        "name": " Trika Technologies PVT.LTD",
+        "position": "Full Stack Developer",
+        "startDate": "2023-05-23",
+        "endDate": "Continuing",
+        "responsibilities": [
+          "Built RESTful APIs using Express.js and MongoDB for data storage",
+          "Implemented authentication and authorization mechanisms for secure user access",
+          "Worked on the migration of legacy systems to modern architectures"
+        ]
+      }
+  };
+  
+  export default experience;
+  
